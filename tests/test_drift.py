227607cef7fbaeaf950ebd127f9e496f1a7d63b0
danth6961 <donald.anthony@gmail.com>
2026-06-29 13:51:23 -0400

    Delete tests directory

--- tests/test_drift.py
+++
@@ -1,7 +0,0 @@
-from engine import cumulative_alloc_drift, max_alloc_drift
-
-def test_drift_functions():
-    current = {"G": 40, "C": 30, "I": 20, "S": 5, "F": 5}
-    target = {"G": 35, "C": 35, "I": 20, "S": 5, "F": 5}
-    assert max_alloc_drift(current, target) == 5
-    assert cumulative_alloc_drift(current, target) == 5
--- tests/test_storage.py
+++
@@ -1,7 +0,0 @@
-from storage import default_state, default_config
-
-def test_defaults_exist():
-    s = default_state()
-    c = default_config()
-    assert "month" in s
-    assert "current_alloc" in c
